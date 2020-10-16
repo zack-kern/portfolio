cde992de7695cd8b60dece530ff07d411046a467
Zack Kern <zkkern@gmail.com>
2020-10-16 00:08:17 -0700

    Bouncing headers can be displayed

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 var header = document.querySelector('header')
 let mouseDiv = document.getElementById('mouseDiv');
+
 const onMouseMove = (e) => {
   mouseDiv.style.left = e.pageX + 'px';
   mouseDiv.style.top = e.pageY + 'px';
 }
 document.addEventListener('mousemove', onMouseMove);
+
+// timeout so the cursor border hides after 0.5s
+var moveTimeout;
+document.onmousemove = () => {
+  mouseDiv.style.display = 'block'
+  clearTimeout(moveTimeout);
+  moveTimeout = setTimeout(() => { mouseDiv.style.display = 'none' }, 500);
+}
